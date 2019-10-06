3149a49be53cab723e7d62e87e55475a26dee2ba
Agni Earasi <earasiar@vcu.edu>
2019-10-06 11:03:39 -0400

    Initial Commit

---
+++ INFO450FINDGOLD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INFO450FINDGOLD
+//
+//  Created by Agni Earasi on 10/6/19.
+//  Copyright © 2019 Agni Earasi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
